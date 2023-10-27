bb86f501fc2438ab713f58032d36fa532dd37501
Luis Thieme <guestuser@MacBook-Pro-von-GuestUser.local>
2023-10-27 22:51:21 +0200

    test img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
         with the tech crowd, I am all in. Lets keep innovating and making tech
         magic happen!
       </p>
+      <img src="/static/images/jumping_luis.png" alt="luis" />
     </div>
   );
 }
