72980a2a2d6c1113a97101a9cace65569f49722e
Luis Thieme <guestuser@MacBook-Pro-von-GuestUser.local>
2023-10-27 22:54:01 +0200

    test img02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <div className="w-fit mx-auto mt-[10%] mb-[3%]">
-        <div className="max-w-[370px] mx-auto mt-1/5 bg-no-repeat bg-[center_right] bg-contain bg-[url('/static/images/jumping_luis.PNG')] h-[280px]">
+        <div className="max-w-[370px] mx-auto mt-1/5 bg-no-repeat bg-[center_right] bg-contain bg-[url('./static/images/jumping_luis.PNG')] h-[280px]">
           <h1 className="w-fit p-2 mx-auto my-auto bg-gradient-to-r from-[#bdc3c7] to-[#2c3e50] bg-clip-text text-transparent text-8xl font-extrabold">
             Hey,
           </h1>
