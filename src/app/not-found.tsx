0c828f2c52250678b95d258d946ac097c6df7f5e
Luis Thieme <guestuser@MacBook-Pro-von-GuestUser.local>
2023-10-28 20:43:27 +0200

    fixed go home button on not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,7 +14,7 @@ export default function NotFound() {
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
-              href="#"
+              href="/"
               className="rounded-md bg-gray-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               Go back home
             </Link>
