11bdbfea3afca0e7183daa8da8b77c401548dd1b
Luis Thieme <guestuser@MacBook-Pro-von-GuestUser.local>
2023-10-30 00:47:42 +0100

    fixed escape seq on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ export default function Home() {
   return (
     <div>
       <div className="w-fit mx-auto mt-[10%] mb-[3%]">
-        <div className="phone:hidden max-w-[470px] mx-auto mt-1/5 bg-no-repeat bg-[center_right] bg-contain bg-jumping_luis h-[280px]">
+        <div className="phone:hidden max-w-[480px] mx-auto mt-1/5 bg-no-repeat bg-[center_right_1rem] bg-contain bg-jumping_luis h-[280px]">
           <h1 className="w-fit p-2 mx-auto my-auto bg-gradient-to-r from-[#bdc3c7] to-[#2c3e50] bg-clip-text text-transparent text-9xl font-extrabold">
             Hey,
           </h1>
           <h1 className="w-fit p-2 mx-auto my-auto bg-gradient-to-r from-[#bdc3c7] to-[#2c3e50] bg-clip-text text-transparent text-9xl font-extrabold">
-            Im Luis
+            I&apos;m Luis
           </h1>
         </div>
         <div className="desktop:hidden max-w-[370px] mx-auto mt-1/5 bg-no-repeat bg-[center_right] bg-contain bg-jumping_luis h-[280px]">
@@ -15,17 +15,19 @@ export default function Home() {
             Hey,
           </h1>
           <h1 className="w-fit p-2 mx-auto my-auto bg-gradient-to-r from-[#bdc3c7] to-[#2c3e50] bg-clip-text text-transparent text-8xl font-extrabold">
-            Im Luis
+            I&apos;m Luis
           </h1>
         </div>
       </div>
-      <p className="mx-auto w-3/5 text-center font-bold text-gray-300">
-        Hey there, Im Luis, a software engineer based in Germany. I am all about
-        coding, problem-solving, and creating cool digital stuff. Whether it is
-        building nifty code, working on exciting projects, or just hanging out
-        with the tech crowd, I am all in. Lets keep innovating and making tech
-        magic happen!
+      <p className="mx-auto w-3/5 text-center font-bold text-gray-400">
+        Hey there, I&apos;m Luis, a software engineer based in Germany. I&apos;m
+        all about coding, problem-solving, and creating cool digital stuff.
+        Whether it&apos;s building nifty code, working on exciting projects, or
+        just hanging out with the tech crowd, I&apos;m all in.
       </p>
+      <h2 className="mx-auto w-3/5 text-center text-lg font-extrabold text-gray-300">
+        Let&apos;s keep innovating and making tech magic happen!
+      </h2>
     </div>
   );
 }
