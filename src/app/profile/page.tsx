4ff31c8bddc8296c058beef12f96f1bd55841f0a
Luis Thieme <guestuser@MacBook-Pro-von-GuestUser.local>
2023-10-30 00:32:20 +0100

    minor changes and registration implementation

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -9,10 +9,18 @@ import Loading from "../components/Loading";
 export default function Page() {
   const [isLoading, setIsLoading] = useState(true);
   const [username, setUsername] = useState("");
+  const [accountStatus, setAccountStatus] = useState("");
   const router = useRouter();
 
   useEffect(() => {
     setUsername(pb.authStore.model?.email);
+    setAccountStatus(pb.authStore.model?.verified);
+    if (pb.authStore.model?.verified) {
+      console.log("verified");
+    } else {
+      console.log("not verified");
+      console.log(pb.authStore.model);
+    }
     if (!pb.authStore.isValid) {
       redirect("/login");
     }
@@ -20,7 +28,6 @@ export default function Page() {
   }, []);
 
   function logout() {
-    console.log("logging out...");
     pb.authStore.clear();
     router.push("/login");
   }
@@ -32,6 +39,12 @@ export default function Page() {
   return (
     <div>
       <h1>Name: {username}</h1>
+      <h1>
+        Status:{" "}
+        {accountStatus
+          ? "Verified"
+          : "Not Verified. This process may take some time. Please be patient."}
+      </h1>
       <button onClick={logout}>Log out</button>
     </div>
   );
