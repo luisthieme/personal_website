9c2c4ffc9dd17acfc32c8c8a5d4b50314dbb0850
luisthieme <luis.thieme@5minds.de>
2023-12-14 22:18:51 +0100

    disable autocancellation for pocketbase

--- src/app/lib/pocketbase.ts
+++ src/app/lib/pocketbase.ts
@@ -1,5 +1,6 @@
 import PocketBase from "pocketbase";
 
 const pb = new PocketBase("https://pocketbase-backend.fly.dev");
+pb.autoCancellation(false); //prob not a good idea but a quick fix
 
 export default pb;
